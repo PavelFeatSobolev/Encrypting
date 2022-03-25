e26f7c979a1fa49fbd5627dafdf8c7cbbaf575e6
Pavel <pavel.feat.sobolev@yandex.ru>
2022-03-25 17:09:43 +0300

    fix изменение файла exemple.php

--- exemple.php
+++ exemple.php
@@ -2,22 +2,14 @@
 
 include 'lib/encrypting/autoload.php';
 
-//Хэширование
-//$hashOb = new \Lib\Encrypting\HashLibs\SimpleHash();
-//$hashOb->setOptionsParam('binary_system', true);
-
-//$hash = $hashOb->getHash('мышь');
-
-//$hash2 = $hashOb->getHash('мышь');
-//$hashOb->comparisonHashes($hash, $hash2);
-
+//шифруем данные
 $encrypting = new \Lib\Encrypting\OpenSSLSymmetric();
-$encrypting->setOptionsParam();
-$options = $encrypting->getOptions();
-$crypt = $encrypting->encryptionData('очень пьяная мышь');
+$crypt = $encrypting->encryptionData('Шифруемая строка');
 
+//дешифруем
 $encryptingDec = new \Lib\Encrypting\OpenSSLSymmetric();
 $result = $encryptingDec->decryptionData($crypt);
+
 var_dump($result);
 
 
